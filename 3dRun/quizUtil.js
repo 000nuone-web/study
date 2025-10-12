f2a7d31a4749e5253afc487f02c1b51dcbbdeda0
000nuone-web <000nuone@gmail.com>
2025-10-13 01:40:10 +0900

    Delete 3dRun directory

--- 3dRun/quizUtil.js
+++
@@ -1,6 +0,0 @@
-export function getShuffledChoices(quizItem) {
-  const shuffledWrong = quizItem.wrong.sort(() => Math.random() - 0.5);
-  const selectedWrong = shuffledWrong.slice(0, 2);
-  const choices = [...selectedWrong, quizItem.correct].sort(() => Math.random() - 0.5);
-  return choices;
-}
